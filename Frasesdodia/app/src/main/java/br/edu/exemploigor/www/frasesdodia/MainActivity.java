cd23fe521859929fc8462cd37728cdd6f2203878
Igor Oliveira <igornnt@gmail.com>
2018-05-07 06:56:08 -0300

    App frases do dia

---
+++ Frasesdodia/app/src/main/java/br/edu/exemploigor/www/frasesdodia/MainActivity.java
@@ -0,0 +1,36 @@
+package br.edu.exemploigor.www.frasesdodia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void gerarNovasFases(View view){
+
+        String[] frases = {
+                "Frase 1",
+                "Frase 2",
+                "Frase 3",
+                "Frase 4",
+                "Frase 5",
+                "Frase 6"
+        };
+
+        TextView frase = findViewById(R.id.fraseTela);
+
+        frase.setText(frases[new Random().nextInt(frases.length)]);
+
+    }
+
+
+}
