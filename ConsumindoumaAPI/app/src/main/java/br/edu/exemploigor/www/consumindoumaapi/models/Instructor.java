2c3c452f441a6fa1020686969d60290f44c15068
Igor Oliveira <igornnt@gmail.com>
2018-05-13 18:39:41 -0300

    App consumindo uma API

---
+++ ConsumindoumaAPI/app/src/main/java/br/edu/exemploigor/www/consumindoumaapi/models/Instructor.java
@@ -0,0 +1,10 @@
+package br.edu.exemploigor.www.consumindoumaapi.models;
+
+/**
+ * Created by igorn on 13/05/2018.
+ */
+
+public class Instructor {
+
+    public String name;
+}
---
+++ ConsumindoumaAPI/app/src/main/java/br/edu/exemploigor/www/consumindoumaapi/models/UdacityCatalog.java
@@ -0,0 +1,16 @@
+package br.edu.exemploigor.www.consumindoumaapi.models;
+
+import java.util.List;
+
+/**
+ * Created by igorn on 13/05/2018.
+ */
+
+public class UdacityCatalog {
+
+    // Estrutura que vai expor os dados que eu esou usando
+    // O mesmo nome do recurso
+
+    public List<Course> courses;
+
+}
